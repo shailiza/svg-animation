768d93a0a6575ae28282c27617f571ea30dacd60
Shailiza Manandhar <shailzam@msn.com>
2016-04-12 22:19:31 -0500

    Updated interactive-svg js

--- js/interactive-svg.js
+++ js/interactive-svg.js
@@ -5,8 +5,9 @@ $(function(){
 		$(this).addClass("svgLoaded");
 
 		if(!response){ // Error loading SVG
-			$(this).html('Error loading SVG. Be sure you are running from a the http protocol (not locally) and have read <strong><a href="http://tympanus.net/codrops/?p=13831#the-javascript">this important part of the tutorial</a></strong>');
+			$(this).html('Error loading SVG. Be sure you are running from a the http protocol (not locally) and have read <strong><a href="http://shailiza.com/blog/2016/04/top-5-manufacturing-industries-of-nepal/#note">this important part of the tutorial</a></strong>');
 		}
 
 	});
 });
+
